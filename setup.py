da65598bdcea62a94162b2bee7563736d63d6454
Leopold Talirz <leopold.talirz@gmail.com>
2018-06-21 20:48:01 +0200

    add description for pypi

--- setup.py
+++ setup.py
@@ -7,5 +7,7 @@ if __name__ == '__main__':
     # Provide static information in setup.json
     # such that it can be discovered automatically
     with open('setup.json', 'r') as info:
+        long_description = open('README.md').read(),
+        long_description_content_type = 'text/markdown',
         kwargs = json.load(info)
     setup(packages=find_packages(), **kwargs)
