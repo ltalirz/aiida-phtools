acec3339425fe92d3f55e725a199123de9a1febc
Leopold Talirz <leopold.talirz@gmail.com>
2018-06-21 20:54:21 +0200

    release v0.1.0a1 for pypi

--- setup.py
+++ setup.py
@@ -7,7 +7,9 @@ if __name__ == '__main__':
     # Provide static information in setup.json
     # such that it can be discovered automatically
     with open('setup.json', 'r') as info:
-        long_description = open('README.md').read(),
-        long_description_content_type = 'text/markdown',
         kwargs = json.load(info)
-    setup(packages=find_packages(), **kwargs)
+    setup(
+        packages=find_packages(),
+        long_description=open('README.md').read(),
+        long_description_content_type='text/markdown',
+        **kwargs)
